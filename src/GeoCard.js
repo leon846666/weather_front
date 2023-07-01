0449bbfacbcec7204362649d600cac51eb09bfda
Leo <leon846666@gmail.com>
2023-07-02 05:35:11 +1200

    changed some layout by using ant design, add button to use the user's current location to get the weather

--- src/GeoCard.js
+++ src/GeoCard.js
@@ -1,57 +1,84 @@
 import React from 'react';
 import GooglePlacesAutocomplete from 'react-google-places-autocomplete';
 import { geocodeByPlaceId } from 'react-google-places-autocomplete';
+import { Button, Row, Col } from "antd";
 import axios from 'axios';
 import WeatherCard from './WeatherCard';
 
 class GeoCard extends React.Component {
 
-    constructor(props) {  
+  constructor(props) {
+    super(props);
+    this.handleLocationSelect = this.handleLocationSelect.bind(this);
+    this.state = {
+      weatherData: null,
+    };
+  }
 
-      super(props);
-      this.handleLocationSelect = this.handleLocationSelect.bind(this);
-      this.state = {
-        weatherData: null,
-      };
-    }
-    handleLocationSelect = (location) => {
-      geocodeByPlaceId(location.value.place_id)
+  callApi = (lat, lng) => {
+    console.log("lat: ",lat)
+    console.log("lng: ",lng)
+    axios.get(`${process.env.REACT_APP_API_URL}/api/weather/get_weather_data?lat=${lat}&lng=${lng}`)
+      .then(response => {
+        const items = response.data;
+        if (items) {
+          console.log(items.main)
+          this.setState({ weatherData: items });
+        }
+      });
+  }
+  handleLocationSelect = (location) => {
+    geocodeByPlaceId(location.value.place_id)
       .then(results => {
         const location = results[0].geometry.location;
         console.log("Latitude:", location.lat());
         console.log("Longitude:", location.lng());
-        let lat =  location.lat();
-        let lng =  location.lng();
-
-        axios.get(`${process.env.REACT_APP_API_URL}/api/weather/get_weather_data?lat=${lat}&lng=${lng}`)
-        .then(response => {
-          const items = response.data;
-          console.log("items:", items);
-          if(items){
-            console.log(items.main)
-            this.setState({ weatherData: items });
-          }
-        });
-
+        let lat = location.lat();
+        let lng = location.lng();
+        this.callApi(lat, lng)
       }
       )
       .catch(error => console.error(error));
-    }
-      render() {
-        return (
-      <div>
-      <GooglePlacesAutocomplete
-          selectProps={{
-            onChange: this.handleLocationSelect,
-          }}
-      />
-      <button onClick={this.handleButtonClick}>Use my current location</button>
+  }
 
-      <WeatherCard weatherData={this.state.weatherData} />
+  handleButtonClick = () => {
+    if (navigator.geolocation) {
+      this.setState({ inputValue: null });
 
-    </div>
-     );
+      navigator.geolocation.getCurrentPosition((position) => {
+        console.log(position)
+        const { latitude, longitude } = position.coords;
+        if(!latitude || !longitude){
+          alert("Geolocation is not supported by this browser.");
+        }
+        this.callApi(latitude, longitude)
+
+      }, (error) => {
+        console.error("Error Code = " + error.code + " - " + error.message);
+      });
+    } else {
+      alert("Geolocation is not supported by this browser.");
     }
+  }
+  render() {
+    return (
+      <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+        <Row>
+          <Col span={48} style={{ textAlign: "center", width: '300px' }}>
+            <GooglePlacesAutocomplete
+              selectProps={{
+                value: this.state.inputValue,
+                onChange: this.handleLocationSelect,
+              }}
+
+            />
+            <Button onClick={this.handleButtonClick} style={{ marginTop: '15px', marginBottom: '30px' }}>Use current location</Button>
+            <WeatherCard weatherData={this.state.weatherData} />
+          </Col>
+        </Row>
+      </div>
+    );
+  }
 }
 
 export default GeoCard;
--- src/WeatherCard.js
+++ src/WeatherCard.js
@@ -3,12 +3,13 @@ import { Card, Spin } from 'antd';
 
 const WeatherCard = ({ weatherData }) => {
   if (!weatherData) {
-    return <Spin tip="Loading..." />;
+    return ;
   }
 
   return (
     <Card title={`Weather in ${weatherData.name}`} style={{ width: 300 }}>
       <p>{` ${weatherData.weather[0].main}`}</p>
+      <img src={`http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`} alt="Weather icon" />
       <p>{`Temperature: ${weatherData.main.temp}°C`}</p>
       <p>{`Humidity: ${weatherData.main.humidity}%`}</p>
       <p>{`Wind Speed: ${weatherData.wind.speed} m/s`}</p>
